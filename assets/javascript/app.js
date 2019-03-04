4808cd57a9a0025b0c479d6a785fbbe24d149dda
ctghadiri <ctghadiri@gmail.com>
2019-03-04 14:00:47 -0800

    pseudocode

---
+++ assets/javascript/app.js
@@ -0,0 +1,21 @@
+// header
+// form
+    // search parameters
+        // search term
+        // number of records
+        // start year opt
+        // end year opt
+    // search button
+    // clear button
+// top articles
+
+// ----------------JS---------------------------
+// ajax 
+    // api key: soSpmFc5VJJ8ZM4BLHZdck7h7O23un8T
+    // api link: https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=yourkey
+    // limit: fl=web_url
+    // year: pub_year
+    
+// clear function
+    // .empty()
+
