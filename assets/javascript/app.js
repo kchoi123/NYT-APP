982f42c60ed629ef761381f6dc5253c00e6db2b6
kchoi123 <k3vinch0i@gmail.com>
2019-03-04 14:35:07 -0800

    html from calvin and js var

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,6 +15,13 @@
     // api link: https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=yourkey
     // limit: fl=web_url
     // year: pub_year
+
+var searchWord; // this is the string that we are searching under q
+var apiKey = "soSpmFc5VJJ8ZM4BLHZdck7h7O23un8T"; // this is our API key
+var beginDate; // example: 20120101 starting date
+var endDate; // example: 20120101 end date
+
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchWord + "&begin_date=" + beginDate + "&end_date="+ endDate + "fl=web_url&api-key=" + apiKey
     
 // clear function
     // .empty()
